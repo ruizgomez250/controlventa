2402e99557559f97bfbba108f2bc859d998cf7d1
ruizgomez250 <ruizgomez250@gmail.com>
2025-10-21 08:09:57 -0300

    mensaje

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use App\Http\Controllers\RolController;
-use App\Services\PermisoService;
+use App\services\PermisoService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
