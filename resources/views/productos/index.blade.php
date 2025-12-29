654d4b9256a1382d3ee2b3b94939a115cf057b15
ruizgomez250 <ruizgomez250@gmail.com>
2025-12-29 11:04:19 -0300

    mensaje

--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -30,7 +30,7 @@
                                
                                 <td>{{  $row->unidaddemedida->descripcion }}</td>
                                 <td>{{ $row->descripcion }}</td>
-                                <td>{{ $row->categoriaproducto->descripcion }}</td>
+                                <td>{{ $row->categoriaproducto?->descripcion }}</td>
                                 <td>{{ $row->stock  }}</td>
                                 <td>{{ $row->pcosto }}</td>
                                 <td>{{ $row->pventa }}</td>
