102d946f1c4a066f44b40486b9f1c64e98b91bc0
ruizgomez250 <ruizgomez250@gmail.com>
2026-05-04 10:58:42 -0300

    mensaje

--- app/Http/Controllers/ReporteVentaNuevoController.php
+++ app/Http/Controllers/ReporteVentaNuevoController.php
@@ -2,12 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\helpers\NumberToWords;
 use App\Models\Venta;
 use App\Models\User;
-use App\services\PermisoService;
-use DateTime;
 use TCPDF;
 
 class ReporteVentaNuevoController extends Controller
