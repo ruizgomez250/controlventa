266e4ccfa2b8ef8525d448bc59409d09cdbfe309
ruizgomez250 <ruizgomez250@gmail.com>
2026-05-04 11:17:35 -0300

    mensaje

--- app/Http/Controllers/ReporteVentaNuevoController.php
+++ app/Http/Controllers/ReporteVentaNuevoController.php
@@ -1,9 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use App\Helpers\NumberToWords;
 use App\Models\Venta;
 use App\Models\User;
 use TCPDF;
+
 
 class ReporteVentaNuevoController extends Controller
 {
@@ -126,7 +129,7 @@ class ReporteVentaNuevoController extends Controller
         $pdf->Cell(50, 7, $ventas->count() . ' ventas', 0, 1, 'L');
 
         // Total en letras
-        $formatter = new HelpersNumberToWords();
+        $formatter = new NumberToWords();
         $pdf->Ln(5);
         $pdf->SetFont('helvetica', 'I', 8);
         $pdf->Cell(0, 5, '( ' . $formatter->toWords($totalGeneral, 0) . ' )', 0, 1, 'C');
